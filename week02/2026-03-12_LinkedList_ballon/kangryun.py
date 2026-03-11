dad3a2489022111cc13d610f64852041441fe453
KangRyun <l01077259370@gmail.com>
2026-03-11 20:42:45 +0900

    ✨ Solve: [Week02] 풍선 터뜨리기 (이강륜)
    Solve: [Week02] 풍선 터뜨리기 (이강륜)

--- week02/2026-03-12_LinkedList_ballon/kangryun.py
+++ week02/2026-03-12_LinkedList_ballon/kangryun.py
@@ -2,6 +2,8 @@ from collections import deque
 
 n = int(input())
 arr = list(map(int, input().split()))
+
+# 인덱스 값과 배열 값을 저장
 ballon = deque(enumerate(arr))
 
 result = []
@@ -9,13 +11,13 @@ for x in range(n):
 
     index, paper = ballon.popleft()
     result.append(index+1)
+    # rotate 인자에 음수가 들어가면 배열이 왼쪽으로 회전
+    # 따라서 음수를 붙혀줘야 요구사항대로 출력 가능
     if paper > 0:
-        ballon.rotate(paper - 1)
+        ballon.rotate(-(paper - 1))
     if paper < 0:
-        ballon.rotate(paper)
-    print(result)
-    print(ballon)
-    print(paper)
+        ballon.rotate(-paper)
+
 print(*result)
 
 
